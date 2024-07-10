31377d3b3fb0b88ae4cd69ae93953ebddf32d931
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 17:21:30 +0530

    [Rajesh] Add : Set Symmetric Difference

---
+++ Data_Structure/Set/Nine_Symmetric_Difference.py
@@ -0,0 +1,9 @@
+# 9. Write a Python program to create a symmetric difference.
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+symmetric_difference = set1 ^ set2
+print(symmetric_difference)
+
+symmetric_difference = set1.symmetric_difference(set2)
+print(symmetric_difference)
