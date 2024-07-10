9f0e11c90999f075178013b463a6d7f997ece726
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 16:27:19 +0530

    [Rajesh] Add : Create Set

---
+++ Data_Structure/Set/One_Create_Set.py
@@ -0,0 +1,20 @@
+# 1. Write a Python program to create a set.
+
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+my_set = set([1, 2, 3, 4, 5])
+print(my_set)
+my_set = set("Hello")
+print(my_set)
+my_set = set((1, 2, 3, 4, 5))
+print(my_set)
+my_set = set([1, 2, 3, 4, 5])
+print(my_set)
+my_set = set(range(1, 6))
+print(my_set)
+
+
+
+
+
+
