105bd4bdfd477d3b7a821239b57ea6e9b0565e08
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 17:19:30 +0530

    [Rajesh] Add : Intersection of set

---
+++ Data_Structure/Set/Six_Intersection_Of_Sets.py
@@ -0,0 +1,9 @@
+# 6. Write a Python program to create an intersection of sets.
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+intersection = set1 & set2
+print(intersection)  
+
+intersection = set1.intersection(set2)
+print(intersection)  
