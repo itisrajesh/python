c6245efc590efbafcc810e8517173796154367f1
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 15:40:54 +0530

    [Rajesh] Add : Unpack Tuple

---
+++ Data_Structure/Tuple/Three_Unpack_Tuple.py
@@ -0,0 +1,8 @@
+# 3. Write a Python program to unpack a tuple in several variables.
+
+my_tuple = (1, 2, 3, 4, 5)
+
+# Unpack the tuple into separate variables
+var1, var2, var3, var4, var5 = my_tuple
+
+print(var1, var2, var3, var4, var5)
