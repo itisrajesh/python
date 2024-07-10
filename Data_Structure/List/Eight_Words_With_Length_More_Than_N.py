00f19600bb8cd22dfba1ca657030304ba2e2a28f
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 13:32:57 +0530

    [Rajesh] Add : Eight -list of words that are longer than n from a given list

--- Data_Structure/List/Eight_Words_With_Length_More_Than_N.py
+++ Data_Structure/List/Eight_Words_With_Length_More_Than_N.py
@@ -1,4 +1,9 @@
 def get_words_longer_than_n(words, n):
+    """
+    Description: list of words that are longer than n from a given list of words.
+    Input: {words: list , n: integer}
+    Output: {list : list}
+    """
     return [word for word in words if len(word) > n]
 
 if __name__ == "__main__":
