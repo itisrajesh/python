7b20fbc86fbb25fb995724484204294c8fd8169d
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 17:20:45 +0530

    [Rajesh] Add : Set Difference

---
+++ Data_Structure/Set/Eight_Set_DIfference.py
@@ -0,0 +1,9 @@
+# 8. Write a Python program to create set difference.
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+difference = set1 - set2
+print(difference)
+
+difference = set1.difference(set2)
+print(difference)
