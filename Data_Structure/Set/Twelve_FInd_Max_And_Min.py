bdf380b0da81596b0c33da1b7b91a0170126e0c3
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 17:25:08 +0530

    [Rajesh] Add : Find min max in set

---
+++ Data_Structure/Set/Twelve_FInd_Max_And_Min.py
@@ -0,0 +1,9 @@
+# 12. Write a Python program to find maximum and the minimum value in a set.
+
+my_set = {12, 23, 45, 1, 76, 89}
+
+max_value = max(my_set)
+print("Maximum value:", max_value)  
+
+min_value = min(my_set)
+print("Minimum value:", min_value)  
