4461a2358e6c8d83a4940f26bbd690e49bf55129
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 17:20:00 +0530

    [Rajesh] Add : Union of set

---
+++ Data_Structure/Set/Seven_Union_Of_Set.py
@@ -0,0 +1,9 @@
+# 7. Write a Python program to create a union of sets.
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+union = set1 | set2
+print(union)  
+
+union = set1.union(set2)
+print(union)  
