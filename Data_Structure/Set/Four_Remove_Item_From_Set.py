f814d279661c26a41804b74ed40d1f75a8677ade
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-10 17:17:47 +0530

    [Rajesh] Add : Remove Item From Set

---
+++ Data_Structure/Set/Four_Remove_Item_From_Set.py
@@ -0,0 +1,9 @@
+# 4. Write a Python program to remove item(s) from set
+
+my_set = {1, 2, 3, 4, 5, 6}
+my_set.remove(4)
+print(my_set)  
+my_set.discard(5) 
+print(my_set)  
+my_set.clear()
+print(my_set)  
