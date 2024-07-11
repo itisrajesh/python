6d44c59649f36fd771216c6318d187d80c88e7f5
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-11 10:53:58 +0530

    [Rajesh] Add : Iterate over Dict

---
+++ Data_Structure/Dictionary/Four_Iterate_Over_Dictionary.py
@@ -0,0 +1,6 @@
+# 4. Write a Python program to iterate over dictionaries using for loops.
+
+student = {'name': 'John', 'age': 20, 'courses': ['Math', 'Science', 'History']}
+
+for key, value in student.items():
+    print(f"Key: {key}, Value: {value}")
