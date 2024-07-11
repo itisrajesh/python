9ff12b3037b096c3b1c790b19fa3b9f91ad6708c
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-11 10:55:27 +0530

    [Rajesh] Add : Printing Unique Value

---
+++ Data_Structure/Dictionary/Seven_Print_Unique_Value.py
@@ -0,0 +1,11 @@
+# 7. Write a Python program to print all unique values in a dictionary.
+# Sample Data : [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}]
+# Expected Output : Unique Values: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+data = [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}]
+
+values = [list(d.values())[0] for d in data]
+
+unique_values = list(set(values))
+
+print("Unique Values:", set(unique_values))
